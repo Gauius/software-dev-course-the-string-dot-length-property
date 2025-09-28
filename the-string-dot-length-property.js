67b5b30d123c64470e7e86647536d13698e0e73d
Gauius <bill.resip@gmail.con>
2025-09-28 10:44:02 +0800

    practice use of String .length method

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -20,11 +20,15 @@ let stringTwo = "JavaScript";
 let stringThree = "";
 
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
+let lengthOne; // Length of stringOne
+let lengthTwo; // Length of stringTwo
 let lengthThree; // Length of stringThree
 
 // Your code here
+lengthOne = stringOne.length;
+lengthTwo = stringTwo.length;
+lengthThree = stringThree.length;
 
-
-
+console.log('"' + stringOne + '"', "has length of", lengthOne);
+console.log('"' + stringTwo + '"', "has length of", lengthTwo);
+console.log('"' + stringThree + '"', "has length of", lengthThree);
